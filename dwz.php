e88255ce89e303c3112faaa508789fcfa799d0c3
ChenPresident <117016446+ChenPresident@users.noreply.github.com>
2022-11-17 23:40:22 +0800

    Update dwz.php

--- dwz.php
+++ dwz.php
@@ -3,6 +3,6 @@
             $array=explode('/', $m3u8);
             $uniName = $array[4];
             $key = str_ireplace(".m3u8","",$uniName);
-            $url = "http://cxz.github.io/".$key; //修改播放地址域名
+            $url = "http://chenpresident.github.io/ccc/".$key; //修改播放地址域名
 			echo json_encode(['code'=>0,'url'=>$url,'m3u8'=>$m3u8]);
 ?>
